a95e6244451f1ca2c5563332dab22a7a3c32bbc8
sheheryar-78 <122275175+sheheryar-78@users.noreply.github.com>
2026-03-02 20:23:27 +0500

    Mine

--- TestFile.py
+++ TestFile.py
@@ -1,1 +1,13 @@
-print("Sheheryar")
+import numpy as np
+# li=[1,2,3,4,5,6,7,8,9]
+# print(type(li))
+# ar=np.array([1,2,3,4,5])
+# print(type(ar))
+#
+# li=[1,2,3,4,5,6,7,8,9]
+# arr=np.array(li)
+# print(arr/2)
+arr=np.array(range(18))
+arr=arr.reshape((3,6))
+print(arr)
+arr.sha
