f1b6c6169508a53a177aec0f9c775e6cdaf2d881
sheheryar-78 <122275175+sheheryar-78@users.noreply.github.com>
2026-03-09 14:02:59 +0500

    Assignment

--- TestFile.py
+++ TestFile.py
@@ -7,7 +7,16 @@ import numpy as np
 # li=[1,2,3,4,5,6,7,8,9]
 # arr=np.array(li)
 # print(arr/2)
-arr=np.array(range(18))
-arr=arr.reshape((3,6))
-print(arr)
-arr.sha
+# arr=np.array(range(18))
+# arr=arr.reshape((3,6))
+# print(arr)
+
+#csv file is comma separated values
+
+import pandas as pd
+sr=pd.Series(['1','2','3'])
+print(sr)
+
+userData={'name':['Sheheryar','Ali','Subhani'],'age':[21,23,20]}
+df=pd.DataFrame(userData)
+print(df)
